19bfa79203c6844b6a5927dbdbce9575452fad6f
Kinshuk <shivisk@gmail.com>
2026-04-02 17:45:52 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -46,6 +47,7 @@ export default function RootLayout({
             </a>
           </p>
         </footer>
+        <Analytics />
       </body>
     </html>
   );
